4cfd27e46eaa7c9a653505a20e08ddbd6ab936c1
sayunlopchan <sayunlopchan01+github@gmail.com>
2024-09-03 12:17:04 +0545

    s

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -6,10 +6,10 @@ const Booking = () => {
       <p className='text-sm md:text-xl lg:text-2xl'>
         Empower Young Minds with Art, Dance, Yoga, and More. Ready to Sparkle?
       </p>
-      <span
-        className=' text-sm md:text-lg px-3 py-2 rounded-2xl border-2 border-white hover:bg-orange-500 transition-all'>
-        <a href="https://wa.me/9857049590" target="_blank"> Join Us Today!</a>
-      </span>
+      <a href="https://wa.me/9857049590"
+        target="_blank"
+        className=' text-sm md:text-lg px-3 py-2 rounded-2xl border-2 border-white hover:bg-orange-500 transition-all'
+      > Join Us Today!</a>
     </div>
   )
 }
